44f448c2261ff33b91e768f752e89b1894fb245a
JorgeArno <jorge.arno@hotmail.com>
2017-04-13 00:42:21 -0400

    All
    Firts Up

---
+++ Entity/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity
+{
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public String title { get; set; }
+        [Required]
+        public short time { get; set; }
+        public short year { get; set; }
+    }
+}
---
+++ MyContext/ContextDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Entity;
+
+namespace MyContext
+{
+    public class ContextDB : DbContext
+    {
+       public DbSet<Movie> Movies { get; set; }
+    }
+}
